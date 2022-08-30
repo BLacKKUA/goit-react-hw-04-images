72f0ad97b7b5172e39ec3336f166cb5aa5920231
Roman Valko <rvalko.dev@gmail.com>
2022-08-30 16:42:20 +0300

    done

--- src/components/ImageGallary/ImageGallery.jsx
+++ src/components/ImageGallary/ImageGallery.jsx
@@ -4,10 +4,11 @@ import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
 import { Circles } from 'react-loader-spinner';
 import { ButtonLoadMore } from '../LoadMore/LoadMore';
 import Modal from '../Modal/Modal';
+import FetchData from '../../FetchData/FetchData';
 
 export default class ImageGallery extends Component {
   state = {
-    gallery: '',
+    gallery: [],
     page: 1,
     loading: false,
     modal: false,
@@ -27,34 +28,32 @@ export default class ImageGallery extends Component {
     this.setState({ currentUrl: webformatURL, currentTags: tags });
   };
 
-  fetchData = () => {
-    fetch(
-      `https://pixabay.com/api/?q=${this.props.inputFilter}&page=${this.state.page}&key=28372607-30c2f074d06c20b95d41a8fad&image_type=photo&orientation=horizontal&per_page=12`
-    )
-      .then(res => {
-        if (res.ok) {
-          return res.json();
-        }
-      })
-      .then(gallery => {
-        this.setState(prevState => ({
-          gallery: [...prevState.gallery, ...gallery.hits],
-          loading: false,
-          total: prevState.total + gallery.hits.length,
-          totalHits: gallery.totalHits,
-        }));
-      })
-      .catch(error => this.setState({ error }));
-  };
-
   componentDidUpdate(prevProps, prevState) {
     if (prevState.page !== this.state.page) {
       this.setState({ loading: true });
-      this.fetchData();
+      FetchData(this.props.inputFilter, this.state.page)
+        .then(gallery => {
+          this.setState(prevState => ({
+            gallery: [...prevState.gallery, ...gallery.hits],
+            loading: false,
+            total: prevState.total + gallery.hits.length,
+            totalHits: gallery.totalHits,
+          }));
+        })
+        .catch(error => this.setState({ error }));
     }
     if (prevProps.inputFilter !== this.props.inputFilter) {
-      this.setState({ loading: true, gallery: '', page: 1, total: 0 });
-      this.fetchData();
+      this.setState({ loading: true, gallery: [], page: 1, total: 0 });
+      FetchData(this.props.inputFilter, this.state.page)
+        .then(gallery => {
+          this.setState(prevState => ({
+            gallery: [...prevState.gallery, ...gallery.hits],
+            loading: false,
+            total: prevState.total + gallery.hits.length,
+            totalHits: gallery.totalHits,
+          }));
+        })
+        .catch(error => this.setState({ error }));
     }
   }
   loadMore = () => {
