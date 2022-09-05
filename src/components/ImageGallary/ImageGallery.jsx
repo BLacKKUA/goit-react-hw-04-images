9de6573b88377b9a6ea624580923c201798ce368
Roman Valko <rvalko.dev@gmail.com>
2022-09-05 16:08:48 +0300

    done

--- src/components/ImageGallary/ImageGallery.jsx
+++ src/components/ImageGallary/ImageGallery.jsx
@@ -1,10 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+import { ButtonLoadMore } from '../LoadMore/LoadMore';
+import { Circles } from 'react-loader-spinner';
+import FetchData from '../../FetchData/FetchData';
 import { GalleryImage } from './ImageGallery.styled';
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
-import { Circles } from 'react-loader-spinner';
-import { ButtonLoadMore } from '../LoadMore/LoadMore';
 import Modal from '../Modal/Modal';
-import FetchData from '../../FetchData/FetchData';
 
 const ImageGallery = ({ inputFilter }) => {
   const [gallery, setGallery] = useState([]);
@@ -14,9 +15,29 @@ const ImageGallery = ({ inputFilter }) => {
   const [currentUrl, setCurrentUrl] = useState('');
   const [currentTags, setCurrentTags] = useState('');
   const [total, setTotal] = useState(0);
-  const [totalHits, setTotalHits] = useState(0);
-  const [firstStart, setFirstStart] = useState(true);
-  const [test, setTest] = useState('');
+  const [query, setQuery] = useState('');
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    if (inputFilter !== query) {
+      setGallery([]);
+    }
+    setQuery(inputFilter);
+  }, [inputFilter, query]);
+
+  useEffect(() => {
+    if (query) {
+      FetchData(query, page).then(gallery => {
+        setGallery(prevHits => [...prevHits, ...gallery.hits]);
+        setLoading(false);
+        setTotal(gallery.total);
+        if (!gallery.total) {
+          setError(`Maybe u made miss? We didn't find photos with this name
+          ${query}`);
+        }
+      });
+    }
+  }, [query, page]);
 
   const toggleModal = () => {
     setModal(!modal);
@@ -27,29 +48,12 @@ const ImageGallery = ({ inputFilter }) => {
     setCurrentTags(tags);
   };
 
-  useEffect(() => {
-    if (test !== inputFilter) {
-      setGallery([]);
-      setPage(1);
-      setTotal(0);
-      setTotalHits(0);
-    }
-    if (firstStart !== true) {
-      FetchData(inputFilter, page).then(gallery2 => {
-        setGallery([...gallery2.hits]);
-        setLoading(false);
-        setTotal(total + gallery2.hits.length);
-        setTotalHits(gallery2.totalHits);
-        setTest(inputFilter);
-      });
-    } else {
-      setFirstStart(false);
-    }
-  }, [inputFilter, page]);
-
   const loadMore = () => {
     setPage(prevState => prevState + 1);
   };
+
+  const showButton = gallery.length > 0 && total > gallery.length && !loading;
+
   return (
     <GalleryImage className="gallery">
       {loading && (
@@ -63,27 +67,15 @@ const ImageGallery = ({ inputFilter }) => {
           visible={true}
         />
       )}
-      {gallery.total === 0 ? (
-        <h1>
-          Maybe u made miss? We didn't find photos with this name
-          {inputFilter}
-        </h1>
-      ) : null}
-      {gallery === '' ? (
-        <p>Enter name images</p>
-      ) : (
+      {error && <h1> {error} </h1>}
+      {gallery?.map(image => (
         <ImageGalleryItem
-          gallery={gallery}
+          image={image}
           onClickModal={toggleModal}
           getImage={getImage}
         />
-      )}
-
-      {(gallery !== '') & (gallery.total !== 0) & (totalHits !== total) ? (
-        <ButtonLoadMore onClick={loadMore} />
-      ) : (
-        ''
-      )}
+      ))}
+      {showButton && <ButtonLoadMore onClick={loadMore} />}
       {modal && (
         <Modal Url={currentUrl} Tags={currentTags} onClose={toggleModal} />
       )}
--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -3,26 +3,22 @@ import {
   GalleryImageItemImage,
 } from './ImageGalleryItem.styled';
 
-const ImageGalleryItem = ({ gallery, onClickModal, getImage }) => {
-  // console.log(gallery);
-  return gallery.map(data => {
-    // console.log(data);
-    return (
-      <GalleryImageItem
-        key={data.id}
-        onClick={() => {
-          onClickModal();
-          getImage(data.webformatURL, data.tags);
-        }}
-      >
-        <GalleryImageItemImage
-          src={data.webformatURL}
-          alt={data.tags}
-          width="180"
-        />
-      </GalleryImageItem>
-    );
-  });
+const ImageGalleryItem = ({ image, onClickModal, getImage }) => {
+  return (
+    <GalleryImageItem
+      key={image.id}
+      onClick={() => {
+        onClickModal();
+        getImage(image.webformatURL, image.tags);
+      }}
+    >
+      <GalleryImageItemImage
+        src={image.webformatURL}
+        alt={image.tags}
+        width="180"
+      />
+    </GalleryImageItem>
+  );
 };
 
 export default ImageGalleryItem;
